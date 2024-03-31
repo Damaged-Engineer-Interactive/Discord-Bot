7cec2517c5e2c25eba3f94ea039f3c05ccdcbdb3
funkyidol <81685737+funkyidol6@users.noreply.github.com>
2024-04-01 00:52:20 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,14 @@
-from nextcord import Interaction, SlashOption
+from nextcord import Interaction, SlashOption, Member
 from nextcord.ext import commands
+from nextcord.ext.commands import has_permissions,MissingPermissions
+import nextcord
 
-bot = commands.Bot()
+TESTING_GUILD_ID = 1224008327621513316  # Replace with your testing guild id
 
+
+intents = nextcord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!",intents=intents)
 
 # command will be global if guild_ids is not specified
 @bot.slash_command(description="Ping command")
@@ -10,18 +16,22 @@ async def ping(interaction: Interaction):
     await interaction.response.send_message("Pong!")
 
 
-@bot.slash_command(name='Kick',description='Kick a member')
+
+
+@bot.slash_command(description='Kick a member')
 @has_permissions(kick_members=True)
 async def kick(ctx,member:nextcord.Member,*,reason = None):
     await member.kick(reason=reason)
     await ctx.send(f"user {member} has been kicked")
-
 
 @kick.error
 async def kick_error(ctx,error):
     if isinstance(error,commands.MissingPermissions):
         await ctx.send("you dont have permission to kick")
 
+
+
+bot.run("MTIyNDAwMzU2NDUzNjMzNjQ0Ng.GByzHO.cPPb2hKY8g3F0MXFJ-8M8mUrno-phUrJRKsOC4")
 
 
 bot.run("MTIyNDAwMzU2NDUzNjMzNjQ0Ng.GByzHO.cPPb2hKY8g3F0MXFJ-8M8mUrno-phUrJRKsOC4")
